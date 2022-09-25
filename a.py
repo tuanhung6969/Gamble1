137e9d84207e06906a8f95ce4bfb55f41e21982b
tuanhung6969 <huyservicecskh@gmail.com>
2022-09-25 17:07:16 +0000

    hi

--- a.py
+++ a.py
@@ -1,36 +1,55 @@
+#idea:HungTuan
+#COde:HungTuan
+while True:
+    import calendar
 
-#gửi những thg lol click vào link bố gửi trên mess này
-#dưới đây là Code của bố, không ăn cắp ăn trộm ở đâu nhé
-#Đọc không hiểu j thì install Python 3.10.4 về nhé
-#Đêm nay con Vong Hồn add mày trên fb ấy, acp nó nhé!!!!!!!!
-#         ----NTH---- 
 
-from random import randint
+    from random import randint
 # list là những con số :>>
-list = []
+    list = []
 
 # k là số con lô
-k = int(input())
+    print("lưu ý cái trò này chỉ là dự đoán thoii, nếu mà thiêng thì biết đâu ăn đc thì sao :>>")
+    k = int(input("nay bà ở đầu ngõ có mấy con lô, type vào đây :"))
+    
+    for i in range(k):
+        list.append(int(input()))
+    print("Bảng vừa nhập nè")
+    print(list)
 
-for i in range(k):
-    list.append(int(input()))
-print("Bảng vừa nhập nè")
-print(list)
+    you = int(input('nhập con lô '))
 
-you = int(input('nhập con lô '))
+    ran = randint(1,2,)
+    a = "Chúc mừng bạn nhé, bạn đã trúng, truy cập https://www.youtube.com/watch?v=OXDRWWzESww để nhận thưởng "
+    b = "Hazzzz không trúng, nhưng mà ksao cả, còn thở là còn gỡ, chạy lại chương trình đi !!!"
+    c = 0
+    while c < k:
+        c+= 1
+        if ran==c:
+            computer=list[c-1]
 
-ran = randint(1,2,)
-a = "Chúc mừng bạn nhé, bạn đã trúng, truy cập https://www.youtube.com/watch?v=OXDRWWzESww để nhận thưởng "
-b = "Hazzzz không trúng, nhưng mà ksao cả, còn thở là còn gỡ, chạy lại chương trình đi !!!"
-c = 0
-while c < k:
-    c+= 1
-    if ran==c:
-        computer=list[c-1]
-print("Bạn vừa nhập : " + str(you))
-print("Kết quả là : " + str(computer))
 
-if you == computer:
-    print(a)
-if you != computer:
-    print(b)
+    print("+-----------------------------------------------------------------+")
+    from datetime import date
+    today = str(date.today())
+    print("Hôm nay là ngày " + today)  
+    print("Bạn vừa nhập : " + str(you))
+    print("Kết quả là : " + str(computer))
+
+    if you == computer:
+        print(a)
+    if you != computer:
+        print(b)
+
+
+    # main program
+    while True:
+        answer = str(input('Gỡ lại không :) y là chạy lại/ n là out :>> '))
+        if answer in ('y', 'n'):
+            break
+        print("Chấm hỏi m vừa nhập cái jz ??.")
+    if answer == 'y':
+        continue
+    else:
+        print("Goodbye")
+        break
